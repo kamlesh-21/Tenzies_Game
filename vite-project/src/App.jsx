6557d15bf187aa4084f105a350c16cfa856bb3ee
kamlesh-21 <kumar2105k@gmail.com>
2023-06-09 00:31:16 +0530

    updated all components

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,108 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    const [rollCount, setRollCount] = React.useState(0);
+
+    
+    
+    const rollCounts = JSON.parse(localStorage.getItem("rollCounts")) || [];
+    const lastRollCount = rollCounts.length > 0 ? rollCounts[rollCounts.length - 1] : 0;
+    const bestScore = rollCounts.length > 0 ? Math.min(...rollCounts) : 0;
+
+    
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+        }
+    }, [dice])
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+            setRollCount(0)
+            const rollCounts = JSON.parse(localStorage.getItem("rollCounts")) || [];
+            rollCounts.push(rollCount);
+            localStorage.setItem("rollCounts", JSON.stringify(rollCounts));
+        }
+        
+        setRollCount(prevCount => prevCount + 1)
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    function clearLocalStorage() {
+        localStorage.removeItem("rollCounts");
+        localStorage.removeItem("rollCount");
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
+            <p>Number of Rolls: {rollCount}</p>
+            <div className="roll-info">
+                <p>Best Score: {bestScore}</p>
+                <p>Last Score: {lastRollCount}</p>
+                <button className="score-data" onClick={clearLocalStorage}>Clear Scores</button>
+            </div>
+        </main>
+    )
+}
